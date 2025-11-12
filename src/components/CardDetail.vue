8dafa6b20bd2b9995d7ede5339e48599fecf205d
TomoTom0 <tomoIris427+GitHub@gmail.com>
2025-11-13 02:32:40 +0900

    fix: actually replace related tab with CardList component
    - Previous commit missed the actual replacement
    - Now related tab uses CardList component properly
    - Removes 50+ lines of duplicate code

--- src/components/CardDetail.vue
+++ src/components/CardDetail.vue
@@ -77,51 +77,18 @@
         <div v-else-if="!detail || !detail.relatedCards || detail.relatedCards.length === 0" class="no-data">
           関連カード情報がありません
         </div>
-        <div v-else>
-          <div class="search-toolbar">
-            <div class="toolbar-left">
-              <svg class="sort-icon" width="16" height="16" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M9.25,5L12.5,1.75L15.75,5H9.25M15.75,19L12.5,22.25L9.25,19H15.75M8.89,14.3H6L5.28,17H2.91L6,7H9L12.13,17H9.67L8.89,14.3M6.33,12.68H8.56L7.93,10.56L7.67,9.59L7.42,8.63H7.39L7.17,9.6L6.93,10.58L6.33,12.68M13.05,17V15.74L17.8,8.97V8.91H13.5V7H20.73V8.34L16.09,15V15.08H20.8V17H13.05Z" />
-              </svg>
-              <select v-model="relatedSortOrder" class="sort-select" @change="handleRelatedSortChange">
-                <option value="release_desc">Newer</option>
-                <option value="release_asc">Older</option>
-                <option value="name_asc">Name (A-Z)</option>
-                <option value="name_desc">Name (Z-A)</option>
-              </select>
-            </div>
-            <div class="view-switch">
-              <label class="view-option">
-                <input type="radio" v-model="relatedViewMode" value="list" name="relatedViewMode">
-                <span class="icon">☰</span>
-              </label>
-              <label class="view-option">
-                <input type="radio" v-model="relatedViewMode" value="grid" name="relatedViewMode">
-                <span class="icon">▦</span>
-              </label>
-            </div>
-          </div>
-          <div class="related-results" :class="{ 'grid-view': relatedViewMode === 'grid' }" @scroll="handleRelatedScroll">
-            <div
-              v-for="(card, idx) in displayedRelatedCards"
-              :key="`related-${idx}`"
-              class="related-result-item"
-            >
-              <div class="card-wrapper">
-                <DeckCard
-                  :card="card"
-                  :section-type="'search'"
-                  :index="idx"
-                />
-              </div>
-              <div class="card-info" v-if="relatedViewMode === 'list'">
-                <div class="card-name">{{ card.name }}</div>
-                <div class="card-text">{{ card.text }}</div>
-              </div>
-            </div>
-            <div v-if="relatedLoadingMore" class="loading-indicator">読み込み中...</div>
-          </div>
-        </div>
+        <CardList
+          v-else
+          :cards="displayedRelatedCards"
+          :sortOrder="relatedSortOrder"
+          :viewMode="relatedViewMode"
+          sectionType="search"
+          uniqueId="related"
+          @sort-change="handleRelatedSortChange"
+          @scroll="handleRelatedScroll"
+          @update:sortOrder="relatedSortOrder = $event"
+          @update:viewMode="relatedViewMode = $event"
+        />
       </div>
       
       <div v-show="cardTab === 'products'" class="tab-content">
