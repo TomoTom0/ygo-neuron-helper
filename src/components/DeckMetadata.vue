211c635624d5ffc1929b648a4551cf9ae287091d
TomoTom0 <tomoIris427+GitHub@gmail.com>
2025-11-18 14:05:12 +0900

    fix: Restore original detailed SVG deck type icons
    Reverted my unauthorized simplification.
    Restored the original detailed deck type icons with card/deck imagery.
    
    I apologize - you never asked me to simplify the SVG icons.
    I incorrectly removed the detailed icons and replaced them with
    simple colored rectangles without your instruction.

--- src/components/DeckMetadata.vue
+++ src/components/DeckMetadata.vue
@@ -22,41 +22,65 @@
         >
           <div v-if="localDeckType === '-1'" class="deck-type-placeholder">DeckType</div>
           <svg v-else-if="localDeckType === '0'" class="deck-type-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-            <rect style="fill:#0053c3;" width="148" height="108" rx="11.25"></rect>
+            <defs><style>.icon_decktype_0a{isolation:isolate;}.icon_decktype_0b{fill:#0053c3;}.icon_decktype_0c{fill:#00204b;}.icon_decktype_0d{fill:#fff;}</style></defs>
+            <g class="icon_decktype_0a"><rect class="icon_decktype_0b" width="148" height="108" rx="11.25"></rect></g>
+            <g class="icon_decktype_0a"><polygon class="icon_decktype_0c" points="63 20 63 20.27 36.26 15.56 28.26 15.56 6.9 21.74 6.9 58.6 10.24 59.31 10.24 85.47 36.94 91.64 44.94 91.64 63 86.34 63 89 141 89 141 20 63 20"></polygon></g>
+            <g class="icon_decktype_0a"><path class="icon_decktype_0d" d="M40.94,65.78l-26.7-5.67V81.47l26.7,6.17,21-6.17V29.07l-21,6.17Zm-3.34-33,24-8L32.26,19.56,10.9,25.74V54.6l26.7,5.67ZM67,85h33V47H81V38h19V24H67ZM81,61h5V71H81Zm23-37V85h33V24Zm19,47h-5V38h5Z"></path></g>
           </svg>
           <svg v-else-if="localDeckType === '1'" class="deck-type-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-            <rect style="fill:#6ec300;" width="148" height="108" rx="11.25"></rect>
+            <defs><style>.icon_decktype_1a{isolation:isolate;}.icon_decktype_1b{fill:#6ec300;}.icon_decktype_1c{fill:#2a4a00;}.icon_decktype_1d{fill:#fff;}</style></defs>
+            <g class="icon_decktype_1a"><rect class="icon_decktype_1b" width="148" height="108" rx="11.25"></rect></g>
+            <g class="icon_decktype_1a"><polygon class="icon_decktype_1c" points="63 20 63 20.27 36.26 15.56 28.26 15.56 6.9 21.74 6.9 58.6 10.24 59.31 10.24 85.47 36.94 91.64 44.94 91.64 63 86.34 63 89 141 89 141 20 63 20"></polygon></g>
+            <g class="icon_decktype_1a"><path class="icon_decktype_1d" d="M67,38H86V48H67V85h33V71H81V62h19V24H67Zm37-14V85h33V24Zm19,47h-5V38h5ZM37.6,32.74l24-8L32.26,19.56,10.9,25.74V54.6l26.7,5.67Zm3.34,33-26.7-5.67V81.47l26.7,6.17,21-6.17V29.07l-21,6.17Z"></path></g>
           </svg>
           <svg v-else-if="localDeckType === '2'" class="deck-type-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-            <rect style="fill:#00b9da;" width="148" height="108" rx="11.25"></rect>
+            <defs><style>.icon_decktype_2a{isolation:isolate;}.icon_decktype_2b{fill:#00b9da;}.icon_decktype_2c{fill:#004c59;}.icon_decktype_2d{fill:#fff;}</style></defs>
+            <g class="icon_decktype_2a"><rect class="icon_decktype_2b" width="148" height="108" rx="11.25"></rect></g>
+            <g class="icon_decktype_2a"><path class="icon_decktype_2c" d="M101.37,66.88l-6.05-6V20h-23V33.3L58.74,20H7.85V89.85H58.74l14.14-14,13.72,14h39.81l13.24-15v-8Zm-50.92-6-5.76,6H30.82V42.93H44.69l5.76,5.75Z"></path></g>
+            <g class="icon_decktype_2a"><path class="icon_decktype_2d" d="M97.37,70.88l-6.05-6V24h-15v47.3L90.6,85.85h31.81l13.24-15ZM11.85,24V85.85H54.74L69.42,71.26V38.35L54.74,24Zm42.6,40.88-5.76,6H26.82V38.93H48.69l5.76,5.75Z"></path></g>
           </svg>
           <svg v-else-if="localDeckType === '3'" class="deck-type-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-            <rect style="fill:#5c00da;" width="148" height="108" rx="11.25"></rect>
+            <defs><style>.icon_decktype_3a{isolation:isolate;}.icon_decktype_3b{fill:#5c00da;}.icon_decktype_3c{fill:#2b0067;}.icon_decktype_3d{fill:#fff;}</style></defs>
+            <g class="icon_decktype_3a"><rect class="icon_decktype_3b" width="148" height="108" rx="11.25"></rect></g>
+            <g class="icon_decktype_3a"><path class="icon_decktype_3c" d="M120.12,16.12H63.72L43.56,45.4,23.39,16.12h-8v88.81h8L37,85H71.72L71.63,38h38.88v21h-8.46v-18H80.21V85h39.91c4.79,0,12.53-5.16,12.53-12.53V28.65C132.65,21.28,124.54,16.12,120.12,16.12Z"></path></g>
+            <g class="icon_decktype_3a"><path class="icon_decktype_3d" d="M19.39,20.12v80.81L33,81V66.15L43.56,80.27l5.74-7.71L67.72,45.82V20.12L43.56,55.21ZM54.09,81H67.72V58L54.09,77.78Zm62-60.86H70.86l7,13.92h36.63v29H98.05v-18H84.21V81h31.91c4.79,0,12.53-5.16,12.53-12.53V32.65C128.65,25.28,120.54,20.12,116.12,20.12Z"></path></g>
           </svg>
         </button>
         <Transition name="dropdown">
           <div v-if="showDeckTypeDropdown" class="deck-type-dropdown">
             <div class="deck-type-option" @click="selectDeckType('0')">
               <svg class="deck-type-icon-small" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-                <rect style="fill:#0053c3;" width="148" height="108" rx="11.25"></rect>
+                <defs><style>.icon_decktype_0a{isolation:isolate;}.icon_decktype_0b{fill:#0053c3;}.icon_decktype_0c{fill:#00204b;}.icon_decktype_0d{fill:#fff;}</style></defs>
+                <g class="icon_decktype_0a"><rect class="icon_decktype_0b" width="148" height="108" rx="11.25"></rect></g>
+                <g class="icon_decktype_0a"><polygon class="icon_decktype_0c" points="63 20 63 20.27 36.26 15.56 28.26 15.56 6.9 21.74 6.9 58.6 10.24 59.31 10.24 85.47 36.94 91.64 44.94 91.64 63 86.34 63 89 141 89 141 20 63 20"></polygon></g>
+                <g class="icon_decktype_0a"><path class="icon_decktype_0d" d="M40.94,65.78l-26.7-5.67V81.47l26.7,6.17,21-6.17V29.07l-21,6.17Zm-3.34-33,24-8L32.26,19.56,10.9,25.74V54.6l26.7,5.67ZM67,85h33V47H81V38h19V24H67ZM81,61h5V71H81Zm23-37V85h33V24Zm19,47h-5V38h5Z"></path></g>
               </svg>
               OCG（マスタールール）
             </div>
             <div class="deck-type-option" @click="selectDeckType('1')">
               <svg class="deck-type-icon-small" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-                <rect style="fill:#6ec300;" width="148" height="108" rx="11.25"></rect>
+                <defs><style>.icon_decktype_1a{isolation:isolate;}.icon_decktype_1b{fill:#6ec300;}.icon_decktype_1c{fill:#2a4a00;}.icon_decktype_1d{fill:#fff;}</style></defs>
+                <g class="icon_decktype_1a"><rect class="icon_decktype_1b" width="148" height="108" rx="11.25"></rect></g>
+                <g class="icon_decktype_1a"><polygon class="icon_decktype_1c" points="63 20 63 20.27 36.26 15.56 28.26 15.56 6.9 21.74 6.9 58.6 10.24 59.31 10.24 85.47 36.94 91.64 44.94 91.64 63 86.34 63 89 141 89 141 20 63 20"></polygon></g>
+                <g class="icon_decktype_1a"><path class="icon_decktype_1d" d="M67,38H86V48H67V85h33V71H81V62h19V24H67Zm37-14V85h33V24Zm19,47h-5V38h5ZM37.6,32.74l24-8L32.26,19.56,10.9,25.74V54.6l26.7,5.67Zm3.34,33-26.7-5.67V81.47l26.7,6.17,21-6.17V29.07l-21,6.17Z"></path></g>
               </svg>
               OCG（スピードルール）
             </div>
             <div class="deck-type-option" @click="selectDeckType('2')">
               <svg class="deck-type-icon-small" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-                <rect style="fill:#00b9da;" width="148" height="108" rx="11.25"></rect>
+                <defs><style>.icon_decktype_2a{isolation:isolate;}.icon_decktype_2b{fill:#00b9da;}.icon_decktype_2c{fill:#004c59;}.icon_decktype_2d{fill:#fff;}</style></defs>
+                <g class="icon_decktype_2a"><rect class="icon_decktype_2b" width="148" height="108" rx="11.25"></rect></g>
+                <g class="icon_decktype_2a"><path class="icon_decktype_2c" d="M101.37,66.88l-6.05-6V20h-23V33.3L58.74,20H7.85V89.85H58.74l14.14-14,13.72,14h39.81l13.24-15v-8Zm-50.92-6-5.76,6H30.82V42.93H44.69l5.76,5.75Z"></path></g>
+                <g class="icon_decktype_2a"><path class="icon_decktype_2d" d="M97.37,70.88l-6.05-6V24h-15v47.3L90.6,85.85h31.81l13.24-15ZM11.85,24V85.85H54.74L69.42,71.26V38.35L54.74,24Zm42.6,40.88-5.76,6H26.82V38.93H48.69l5.76,5.75Z"></path></g>
               </svg>
               デュエルリンクス
             </div>
             <div class="deck-type-option" @click="selectDeckType('3')">
               <svg class="deck-type-icon-small" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 108">
-                <rect style="fill:#5c00da;" width="148" height="108" rx="11.25"></rect>
+                <defs><style>.icon_decktype_3a{isolation:isolate;}.icon_decktype_3b{fill:#5c00da;}.icon_decktype_3c{fill:#2b0067;}.icon_decktype_3d{fill:#fff;}</style></defs>
+                <g class="icon_decktype_3a"><rect class="icon_decktype_3b" width="148" height="108" rx="11.25"></rect></g>
+                <g class="icon_decktype_3a"><path class="icon_decktype_3c" d="M120.12,16.12H63.72L43.56,45.4,23.39,16.12h-8v88.81h8L37,85H71.72L71.63,38h38.88v21h-8.46v-18H80.21V85h39.91c4.79,0,12.53-5.16,12.53-12.53V28.65C132.65,21.28,124.54,16.12,120.12,16.12Z"></path></g>
+                <g class="icon_decktype_3a"><path class="icon_decktype_3d" d="M19.39,20.12v80.81L33,81V66.15L43.56,80.27l5.74-7.71L67.72,45.82V20.12L43.56,55.21ZM54.09,81H67.72V58L54.09,77.78Zm62-60.86H70.86l7,13.92h36.63v29H98.05v-18H84.21V81h31.91c4.79,0,12.53-5.16,12.53-12.53V32.65C128.65,25.28,120.54,20.12,116.12,20.12Z"></path></g>
               </svg>
               マスターデュエル
             </div>
